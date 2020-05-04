6c5b0b68df81196f4785fb9a380a6442e5b43b82
Honcha-renko <o.honcharenko@sloboda-studio.com>
2020-05-04 20:48:50 +0300

    modify tasks_controller

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,2 +1,41 @@
 class TasksController < ApplicationController
+
+  def index
+
+  end
+
+  def show
+    @task = Task.find(params[:id])
+    #render json: Task.all
+  end
+
+  def new
+    puts 'create action...may be'
+  end
+
+  def edit
+
+  end
+
+  def create
+    render plain: params[:task].inspect
+    params.permit!
+    @task = Task.new(params)
+    @task.save
+    redirect_to @task
+  end
+
+  def update
+
+  end
+
+  def destroy
+
+  end
+
+  private
+
+  def params
+    params.require(:task).permit(:title, :text, :tag, :done, :expire_on)
+  end
 end
