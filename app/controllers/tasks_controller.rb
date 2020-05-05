72ea6fd9f4006f89f5af1447cdc0b0a2efc5a54c
Honcha-renko <o.honcharenko@sloboda-studio.com>
2020-05-05 22:24:12 +0300

    modified task_controller. added methods show index destroy

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,41 +1,40 @@
 class TasksController < ApplicationController
 
   def index
+    @tasks = Task.all
+  end
 
+  def new
   end
 
   def show
     @task = Task.find(params[:id])
-    #render json: Task.all
-  end
-
-  def new
-    puts 'create action...may be'
-  end
-
-  def edit
-
   end
 
   def create
-    render plain: params[:task].inspect
-    params.permit!
-    @task = Task.new(params)
+    @task = Task.new(task_params)
     @task.save
     redirect_to @task
   end
 
   def update
-
+    @task = Task.find(params[:id])
+    if @task.update(task_params)
+      redirect_to @task
+    else
+      render 'edit'
+    end
   end
 
   def destroy
-
+    Task.find(params[:id]).destroy
+     redirect_back(fallback_location: root_path)
   end
 
   private
 
-  def params
+  def task_params
+    params['task']['tag'] = params['task']['tag'].to_i
     params.require(:task).permit(:title, :text, :tag, :done, :expire_on)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
+# frozen_string_literal: true
 Rails.application.routes.draw do
-  get 'to_do_list/index'
-  root 'to_do_list#index'
+  root 'tasks#index'
   resources :tasks
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
