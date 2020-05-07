9ffed31675140927a97723c9273f2cdeb9116d18
Honcha-renko <o.honcharenko@sloboda-studio.com>
2020-05-08 00:43:35 +0300

    fix  data check

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -4,15 +4,12 @@ class TasksController < ApplicationController
     @tasks = Task.order(tag: :asc)
   end
 
-  def new
-  end
-
   def show
-    if Task.find_by(id: params[:id]).nil?
+    if Task.exists?(params[:id])
+      @task = Task.find(params[:id])
+    else
       redirect_to tasks_path, message: "User with #{params[:id]} id doesn\'t exist"
-      return
     end
-    @task = Task.find(params[:id])
   end
 
   def create
@@ -21,23 +18,21 @@ class TasksController < ApplicationController
   end
 
   def update
-    if Task.find_by(id: params[:id]).nil?
+    if Task.exists?(params[:id])
+      @task = Task.find(params[:id])
+      redirect_to @task if @task.update(task_params)
+    else
       redirect_to tasks_path, message: "User with #{params[:id]} id doesn\'t exist"
-      return
     end
-
-    @task = Task.find(params[:id])
-    redirect_to @task if @task.update(task_params)
   end
 
   def destroy
-    if Task.find_by(id: params[:id]).nil?
+    if Task.exists?(params[:id])
+      Task.find(params[:id]).destroy
+      redirect_back(fallback_location: tasks_path)
+    else
       redirect_to tasks_path, message: "User with #{params[:id]} id doesn\'t exist"
-      return
     end
-
-    Task.find(params[:id]).destroy
-    redirect_back(fallback_location: tasks_path)
   end
 
   private
