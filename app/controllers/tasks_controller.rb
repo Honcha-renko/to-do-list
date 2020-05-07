9c8bfbe851cd84652ca673e2cffdecf80bf2cb8c
Honcha-renko <o.honcharenko@sloboda-studio.com>
2020-05-07 19:27:48 +0300

    added record check

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -8,6 +8,10 @@ class TasksController < ApplicationController
   end
 
   def show
+    if Task.find_by(id: params[:id]).nil?
+      redirect_to tasks_path, message: "User with #{params[:id]} id doesn\'t exist"
+      return
+    end
     @task = Task.find(params[:id])
   end
 
@@ -17,13 +21,23 @@ class TasksController < ApplicationController
   end
 
   def update
+    if Task.find_by(id: params[:id]).nil?
+      redirect_to tasks_path, message: "User with #{params[:id]} id doesn\'t exist"
+      return
+    end
+
     @task = Task.find(params[:id])
     redirect_to @task if @task.update(task_params)
   end
 
   def destroy
+    if Task.find_by(id: params[:id]).nil?
+      redirect_to tasks_path, message: "User with #{params[:id]} id doesn\'t exist"
+      return
+    end
+
     Task.find(params[:id]).destroy
-    redirect_back(fallback_location: root_path)
+    redirect_back(fallback_location: tasks_path)
   end
 
   private
