a1dc633a80a8efd0681eca0454116175c7c98041
Honcha-renko <o.honcharenko@sloboda-studio.com>
2020-05-09 01:49:22 +0300

    deleted partial, fixed update method

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -2,53 +2,45 @@
 
 # class with CRUD operations
 class TasksController < ApplicationController
+  before_action :find_task, only: %i[show edit destroy]
+
   def index
     @tasks = Task.order(tag: :asc)
   end
 
-  def show
-    if Task.exists?(params[:id])
-      @task = Task.find(params[:id])
-    else
-      redirect_to tasks_path, message: "User with #{params[:id]} id doesn\'t exist"
-    end
-  end
+  def new; end
+
+  def show; end
 
   def create
-    binding.pry
     @task = Task.create(task_params)
+    binding.pry
     redirect_to @task
   end
 
-  before_action :find_task, only: %i[update delete]
   def update
-    if Task.exists?(params[:id])
-      render 'update'
-      #binding.pry
-      redirect_to @task if @task.update(task_params)
-    else
-      redirect_to tasks_path, message: "User with #{params[:id]} id doesn\'t exist"
-    end
+    @task.update(oth_params)
+    redirect_to @task
   end
 
   def destroy
-    if Task.exists?(params[:id])
-      find_task.destroy
-      redirect_back(fallback_location: tasks_path)
-    else
-      redirect_to tasks_path, message: "User with #{params[:id]} id doesn\'t exist"
-    end
+    @task.destroy
+    redirect_back(fallback_location: tasks_path)
   end
 
   private
 
   def find_task
     @task = Task.find(params[:id])
+    redirect_to tasks_path, message: "User with #{params[:id]} id doesn\'t exist" unless Task.exists?(params[:id])
   end
 
   def task_params
-    # binding.pry
     params['task']['tag'] = params['task']['tag'].to_i
     params.require(:task).permit(:title, :text, :tag, :done, :expire_on)
+  end
+
+  def oth_params
+    params.permit(:title, :text, :tag, :done, :expire_on)
   end
 end
